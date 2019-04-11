17c6a83f5e9b2d73b3530c755d213e22f96f88b4
Roberto Arcomano <info@robertoarcomano.it>
2019-04-12 00:04:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # Script to Create a Container for LAMP, using Docker
 
-# 1. Download supervisor.conf
-wget https://raw.githubusercontent.com/robertoarcomano/DockerLamp/master/supervisord.conf -O /tmp/supervisord.conf
+# 1. Download supervisor.conf /tmp/Dockerfile
+wget https://raw.githubusercontent.com/robertoarcomano/DockerLamp/master/supervisord.conf -q -O /tmp/supervisord.conf
+wget https://raw.githubusercontent.com/robertoarcomano/DockerLamp/master/Dockerfile -q -O /tmp/Dockerfile
 
 # 2. Download Dockerfile and create image
-wget https://raw.githubusercontent.com/robertoarcomano/DockerLamp/master/Dockerfile -q -O -|docker build -t bertolinux_lamp - /tmp/
+cd /tmp
+docker build -t bertolinux_lamp .
 
 # 3. Create the container from the image
 docker create -p 81:80 --name "bertolinux_lamp" bertolinux_lamp
@@ -13,5 +15,5 @@ docker create -p 81:80 --name "bertolinux_lamp" bertolinux_lamp
 # 4. Start the container
 docker start bertolinux_lamp
 
-# 5. Delete supervisor.conf
-rm /tmp/supervisord.conf
+# 5. Delete supervisor.conf /tmp/Dockerfile
+rm /tmp/supervisord.conf /tmp/Dockerfile
