fa5a48d33cbb074a04c443d5d58ce78fdec91905
Roberto Arcomano <info@robertoarcomano.it>
2019-04-11 22:18:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,3 @@ docker create --name "bertolinux_lamp" bertolinux_lamp
 
 # 3. Start the container
 docker start bertolinux_lamp
-
-# 4. Connect to the container
-docker exec -it bertolinux_lamp /bin/bash
