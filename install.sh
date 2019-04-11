83ab19deb5079cfb9de14352a171fc2d7fdf63d3
Roberto Arcomano <info@robertoarcomano.it>
2019-04-11 21:20:04 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Download Dockerfile and execute script
+wget -q https://github.com/robertoarcomano/DockerLamp/archive/master.zip
+unzip master.zip
+rm master.zip
+cd DockerLamp-master
+sh ./create_lamp.sh
