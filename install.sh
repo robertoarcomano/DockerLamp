2e90fb0576ade39c02853e19a56f4d8bbde120af
Roberto Arcomano <bertolinux@gmail.com>
2020-05-03 22:58:34 +0100

    Updated install.sh with new branch

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Script to Create a Container for LAMP, using Docker
 
 # 1. Download git repository
-git clone https://github.com/robertoarcomano/DockerLamp.git /tmp/DockerLamp
+git clone -b domain_robertoarcomano https://github.com/robertoarcomano/DockerLamp.git /tmp/DockerLamp
 cd /tmp/DockerLamp
 
 # 2. Download Dockerfile and create image
